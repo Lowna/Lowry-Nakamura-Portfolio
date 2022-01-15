abbfa8914ba12b796be6139bc9b9be9856bc2d2c
Lowry Nakamura <lowry.nakamura@gmail.com>
2022-01-14 16:09:12 -0800

    update app.js add behance portfolio link clean code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
+/* 
+Javascript for lowry nakamura's portfolio
+
+*/
+
 // comment from console log
 console.log(`This Lowry Nakamura's Portfolio site`)
 
+/* 
+
+Toggle nav btn
+
+*/
 // toggle nav
 let toggleNav = document.getElementById('showHide')
 toggleNav.addEventListener('click', function(e) {
@@ -14,6 +24,33 @@ toggleNav.addEventListener('click', function(e) {
     }
 })
 
-// show back to top link
 
-let showTopScroll = document.getElementById('topUp')
+
+/* 
+
+Back to top btn
+
+*/
+// show back to top link
+//Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    mybutton.style.transition = " ease-in-out 1000ms"
+    mybutton.style.color = "#ffffff"
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
